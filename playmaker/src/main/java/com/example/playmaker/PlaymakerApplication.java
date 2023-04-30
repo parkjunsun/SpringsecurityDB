3deaaf6ba50af77ab0f203fae262509bf223a25d
parkjunsun <qkrwnstns52@naver.com>
2023-04-30 15:16:59 +0900

    프로젝트 생성

---
+++ playmaker/src/main/java/com/example/playmaker/PlaymakerApplication.java
@@ -0,0 +1,13 @@
+package com.example.playmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaymakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaymakerApplication.class, args);
+	}
+
+}
---
+++ playmaker/src/test/java/com/example/playmaker/PlaymakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.playmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaymakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
